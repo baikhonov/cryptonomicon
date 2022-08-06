0060ae8a311a898a28bdf4472a6683e708599644
Baikhonov Samrat <baikhonov.samrat@gmail.com>
2022-08-06 13:35:47 +0500

    Вынес график в отдельный компонент

--- src/App.vue
+++ src/App.vue
@@ -92,49 +92,11 @@
         </dl>
         <hr class="w-full border-t border-gray-600 my-4" />
       </template>
-      <section v-if="selectedTicker" class="relative">
-        <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-          {{ selectedTicker.name }} - USD
-        </h3>
-        <div
-          class="flex items-end border-gray-600 border-b border-l h-64"
-          ref="graph"
-        >
-          <div
-            v-for="(bar, idx) in normalizedGraph"
-            :key="idx"
-            :style="{ height: `${bar}%` }"
-            class="bg-purple-800 border w-10"
-          ></div>
-        </div>
-        <button
-          @click="selectedTicker = null"
-          class="absolute top-0 right-0"
-          type="button"
-        >
-          <svg
-            height="30"
-            style="enable-background: new 0 0 512 512"
-            version="1.1"
-            viewBox="0 0 511.76 511.76"
-            width="30"
-            x="0"
-            xml:space="preserve"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:svgjs="http://svgjs.com/svgjs"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            y="0"
-          >
-            <g>
-              <path
-                d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
-                data-original="#000000"
-                fill="#718096"
-              ></path>
-            </g>
-          </svg>
-        </button>
-      </section>
+      <crypto-chart
+        :selectedTicker="selectedTicker"
+        :graph="graph"
+        @hide-chart="hideChart"
+      />
     </div>
   </div>
 </template>
@@ -158,11 +120,13 @@
 // [х] При удалении тикера остаётся выбор и вывод графика
 import { subscribeToTicker, unsubscribeFromTicker } from "@/api";
 import AddTicker from "@/components/AddTicker";
+import CryptoChart from "@/components/CryptoChart";
 
 export default {
   name: "App",
   components: {
     AddTicker,
+    CryptoChart,
   },
   data() {
     return {
@@ -172,7 +136,6 @@ export default {
       selectedTicker: null,
 
       graph: [],
-      maxGraphElements: 1,
 
       page: 1,
 
@@ -202,12 +165,7 @@ export default {
       });
     }
   },
-  mounted() {
-    window.addEventListener("resize", this.calculateMaxGraphElements);
-  },
-  beforeUnmount() {
-    window.removeEventListener("resize", this.calculateMaxGraphElements);
-  },
+
   computed: {
     tooManyTickersAdded() {
       return this.tickers.length > 4;
@@ -230,23 +188,6 @@ export default {
       return this.filteredTickers.length > this.endIndex;
     },
 
-    limitedAvailableStickers() {
-      return 0;
-    },
-
-    normalizedGraph() {
-      const maxValue = Math.max(...this.graph);
-      const minValue = Math.min(...this.graph);
-
-      if (maxValue === minValue) {
-        return this.graph.map(() => 50);
-      }
-
-      return this.graph.map(
-        (price) => 5 + ((price - minValue) * 95) / (maxValue - minValue)
-      );
-    },
-
     pageStateOptions() {
       return {
         filter: this.filter,
@@ -255,12 +196,6 @@ export default {
     },
   },
   methods: {
-    calculateMaxGraphElements() {
-      if (!this.$refs.graph) {
-        return;
-      }
-      this.maxGraphElements = this.$refs.graph.clientWidth / 38;
-    },
     updateTicker(tickerName, price) {
       this.tickers
         .filter((t) => t.name === tickerName)
@@ -305,6 +240,9 @@ export default {
         this.selectedTicker = null;
       }
       unsubscribeFromTicker(tickerToRemove.name);
+    },
+    hideChart() {
+      this.selectedTicker = null;
     },
   },
 
